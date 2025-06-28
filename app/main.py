0e29b51c6946230c3b6cb2c8282a215e45cf3118
hyejinggu <118355536+hyejinggu@users.noreply.github.com>
2025-06-28 11:31:24 +0900

    chore: CORS middleware 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.api import emotion, qr
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # 또는 ["*"]로 전체 허용 가능
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 서버 실행: uvicorn app.main:app --reload
 @app.get("/")
